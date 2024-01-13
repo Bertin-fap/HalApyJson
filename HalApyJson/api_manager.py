ead32085f7e2ef98cad62176fac316adabd70ad6
Bertin-fap <francois.bertin7@wanadoo.fr>
2024-01-13 20:38:14 +0100

    add docstring

--- HalApyJson/api_manager.py
+++ HalApyJson/api_manager.py
@@ -1,6 +1,16 @@
 __all__ = ['hal_query',]
 
 def hal_query(year, institute):
+    '''The `hal_query` function queryies the HAL database via its api.
+        
+    Args:
+        year (str): the yera to query.
+        institute (str) : the institute to query
+    
+    Returns
+        (Panndas data frame) : The parsed json answer to the query.
+        
+    '''
 
     #https://realpython.com/python-requests/#getting-started-with-requests
     
@@ -13,7 +23,7 @@ def hal_query(year, institute):
     from HalApyJson.json_parser import parse_json
    
     # Setting hal API
-    hal_api = _set_hal_api_(year, institute)
+    hal_api = _set_hal_api(year, institute)
     
     # Get the request response
     try:
@@ -34,14 +44,30 @@ def hal_query(year, institute):
     return results_df   
 
     
-def _set_hal_api_(year, institute):
+def _set_hal_api(year, institute):
+
+    '''The `_set_hal_api` function buids the request to send to the HAL api:
+        
+    Args:
+        year (str): the year of interest.
+        institute (str) : the institute to query.
+        
+    Returns
+        (str) : the request.
+        
+    Notes:
+        The globals 'GLOBAL' module of `GLOBALS` package is used.
+        
+    '''
     
+    # Standard library imports
     from string import Template
     
+    # Internal import
     from HalApyJson.GLOBALS import GLOBAL
 
     
-    dict_param_quaery = dict( 
+    dict_param_query = dict( 
                              query_header = GLOBAL['HAL_URL'] + GLOBAL['HAL_GATE'] + '/?q=', 
                              query = GLOBAL['QUERY_TERMS'],
                              HAL_RESULTS_NB     = GLOBAL['HAL_RESULTS_NB'] ,  # default=30; maximum= 10000
@@ -64,5 +90,5 @@ def _set_hal_api_(year, institute):
                      "&indent=true")
                     )
     
-    hal_api = query.safe_substitute(dict_param_quaery)
+    hal_api = query.safe_substitute(dict_param_query)
     return hal_api
--- HalApyJson/json_parser.py
+++ HalApyJson/json_parser.py
@@ -1,36 +1,42 @@
 __all__ = ['parse_json',]
 
-def parse_json (response):
+def parse_json(response):
+
+    '''The `parse_json` function parses the dict response associoated with the json answer to the request.
+            
+        
+    Args:
+        responce (dict): the dict to be parsed.
+    
+    Returns
+        (Pandas Data Frame) : The dataframe resulting from the parsed dict.
+        
+    Notes:
+        The globals 'GLOBAL' from the `GLOBALS` module is used.
+        
+    '''
     
     # 3rd party import
     import pandas as pd
     
+    # Internal import
     from HalApyJson.GLOBALS import GLOBAL
-    
-    def _get_field_value(txt, field_key):
-        try:
-            field_value = txt[GLOBAL['HAL_FIELDS'][field_key]]
-        except KeyError:
-            field_value= 'NA'                
-        return field_value
 
-    # Initializing results_df
-    fields_keys_select = GLOBAL['HAL_FIELDS'].keys() #FIELDS_KEYS_SELECT
-   
     parsed_response = response.json()
     parsed_response = parsed_response['response']
     items_nbr = parsed_response['numFound']
     print('Number of items: ',items_nbr)
     
-    results_df = pd.DataFrame(columns = fields_keys_select)
+    fields_keys_select = GLOBAL['HAL_FIELDS'] #FIELDS_KEYS_SELECT
+    results_df = pd.DataFrame(columns = fields_keys_select.keys())
     for txt in parsed_response['docs']:
-        num_df = pd.DataFrame(columns = fields_keys_select)
+        num_df = pd.DataFrame(columns = fields_keys_select.keys())
         num_results_dict = {}
-        for k in fields_keys_select: 
-            field_value = _get_field_value(txt, k)
-            num_results_dict[k] = [field_value]  
+        for k,v in fields_keys_select.items(): 
+            field_value = txt.get(v,'NA')
+            if isinstance(field_value,list) : field_value= ','.join(field_value)
+            num_results_dict[k] = [field_value] 
         num_df = pd.DataFrame.from_dict(num_results_dict, orient = 'columns')
         results_df = pd.concat([results_df, num_df], ignore_index = True)
     results_df.rename(columns = GLOBAL['HAL_FINAL_COLS'], inplace = True)
-
     return results_df
